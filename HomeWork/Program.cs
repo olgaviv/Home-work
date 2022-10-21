90c7c34a52d521d69ab4e9279313b002e02336e2
olga <olgaviv@mail.ru>
2022-10-21 13:42:17 +0700

    add home work2

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 
 int TwoNumber(int number)
 {
@@ -17,4 +17,38 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 int result = TwoNumber(n);
 Console.WriteLine($"Two digital of a number {n} is {result}");
+
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Dif (int n)
+{
+        while (n>=999)
+        {
+        
+            n = n/10;
+            
+        }
+    n = n % 10;
+
+    return n;        
+
+    }
+
+
+
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <100)
+{
+    Console.WriteLine("Введенное число не имеет третьей цифры");
+
+}
+
+int Resultat = Dif (number);
+
+Console.WriteLine($"Третья цифра в числе {number}  будет {Resultat}");
+
+
 
